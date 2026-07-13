c0201c9649640bca76546cbbd6d9f4eee67a4a33
Sanjay Raj <sanjayraj23560@gmail.com>
2026-07-14 01:19:54 +0530

    feat: updated membership payment time windows and translation keys

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendEmail = async (to, subject, html) => {
     try {
         const { data, error } = await resend.emails.send({
-            from: 'Intern Area <saans.duckdns.org>',
+            from: 'Intern Area <onboarding@resend.dev>',
             to,
             subject,
             html,
