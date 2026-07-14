10f3a66f94d8bf9065536308ef238f2fed62f62f
Sanjay Raj <sanjayraj23560@gmail.com>
2026-07-14 08:23:38 +0530

    Latest code push

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,18 +1,19 @@
-import { TransactionalEmailsApi, TransactionalEmailsApiApiKeys, SendSmtpEmail } from '@getbrevo/brevo';
+import { BrevoClient } from '@getbrevo/brevo';
 
-const apiInstance = new TransactionalEmailsApi();
-apiInstance.setApiKey(TransactionalEmailsApiApiKeys.apiKey, process.env.BREVO_API_KEY);
+const brevo = new BrevoClient({
+    apiKey: process.env.BREVO_API_KEY,
+});
 
 export const sendEmail = async (to, subject, html) => {
     try {
-        const sendSmtpEmail = new SendSmtpEmail();
-        sendSmtpEmail.sender = { name: 'Intern Area', email: 'sanjayraj23560@gmail.com' };
-        sendSmtpEmail.to = [{ email: to }];
-        sendSmtpEmail.subject = subject;
-        sendSmtpEmail.htmlContent = html;
+        const data = await brevo.transactionalEmails.sendTransacEmail({
+            subject,
+            htmlContent: html,
+            sender: { name: 'Intern Area', email: 'sanjayraj23560@gmail.com' },
+            to: [{ email: to }],
+        });
 
-        const data = await apiInstance.sendTransacEmail(sendSmtpEmail);
-        console.log("Email sent successfully:", data.body?.messageId);
+        console.log("Email sent successfully:", data.messageId);
         return true;
     } catch (error) {
         console.error("Brevo Delivery Exception:", error.message);
